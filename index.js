c9ab2fb35848d52aa5846570601ee2e8b9e0610e
lakshy <luckyron1279@gmail.com>
2021-07-16 11:00:07 +0530

    change

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ app.use(express.static("./assets"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/views/index.html"));
 });
+app.get("/tour",(req, res) => {
+  res.sendFile(path.join(__dirname + "/views/tours.html"));
+});
+app.get("/blog",(req,res)=> {
+  res.sendFile(path.join(__dirname + "/views/blog.html"));
+});
+app.get("/contact",(req,res)=> {
+  res.sendFile(path.join(__dirname + "/views/contact.html"));
+});
+app.get("/signup",(req,res)=> {
+  res.sendFile(path.join(__dirname + "/views/signup.html"));
+});
 app.listen(port, function (err) {
   if (err) {
     console.log(`error in running the server: ${err}`);
